d6147b53bdea7ca4950eb0b4775197f55cfca195
DmitryPashkevich <dmitry.pashkevich@bk.ru>
2019-02-27 16:14:58 +0300

    End

--- src/index.js
+++ src/index.js
@@ -6,27 +6,18 @@ module.exports = function makeExchange(currency) {
         "D":10, 
         "N":5, 
         "P":1};
-
-
     let twoCoins = {};
-
 
     if(currency>10000){
         return {error: "You are rich, my friend! We don't have so much coins for exchange"};
     } else if(currency<=0){
         return {};
     }
-
     for (i in coins) {
        if(currency >= coins[i]) {
          twoCoins[i] = Math.floor(currency/coins[i]);
          currency =currency - twoCoins[i] * coins[i];
        }
-
-       return twoCoins;
     }
-
-
-
-
+    return twoCoins;
 }
