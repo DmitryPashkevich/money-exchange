dcefd25a9faebab642dbb6a12db5277834799d7d
DmitryPashkevich <dmitry.pashkevich@bk.ru>
2019-02-27 16:11:13 +0300

    End

--- src/index.js
+++ src/index.js
@@ -1,52 +1,32 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    var objectMoney = {
-'H': 0,
-'Q': 0,
-'D': 0,
-'W': 0,
-'P':0
-    };
+    let coins = {
+        "H":50, 
+        "Q":25, 
+        "D":10, 
+        "N":5, 
+        "P":1};
 
-    var noll = {
 
-    }
-    
-    var counter = 0;
-    
-    if (currency <= 0) {
-       return noll;
-    }
+    let twoCoins = {};
 
-    if (currency > 10000) {
-     noll.error ="You are rich, my friend! We don't have so much coins for exchange";
-     return noll;
-    }
- 
-    while (currency >= counter + 50) {
-        counter += 50;
-        objectMoney['H']++;
+
+    if(currency>10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else if(currency<=0){
+        return {};
     }
 
-    while (currency >= conter + 25) {
-        counter += 25;
-        objectMoney['Q']++;
+    for (i in coins) {
+       if(currency >= coins[i]) {
+         twoCoins[i] = Math.floor(currency/coins[i]);
+         currency -= twoCoins[i] * coins[i];
+       }
+
+       return twoCoins;
     }
 
-    while (currency >=counter + 10) {
-        counter += 10;
-        objectMoney['D']++;
-    }
 
-    while (currency >= counter + 5) {
-        counter += 5;
-        objectMoney['W']++;
-    }
 
-    while ( currency != counter ) {
-        counter ++;
-        objectMoney['P']++;
-    }
 
-return objectMoney;
 }
