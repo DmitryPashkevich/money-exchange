8059dc80b0872e34ed61fdd61f5e2bd41d50d6aa
DmitryPashkevich <dmitry.pashkevich@bk.ru>
2019-02-27 14:27:01 +0300

    d

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var objectMoney = {
+
+    };
+    
+    
+    if (currency <= 0) {
+       return objectMoney;
+    }
+
+    if (currency > 10000) {
+     objectMoney.error ="You are rich, my friend! We dont have so much coins for exchange";
+     return objectMoney;
+    }
+
+
 }
